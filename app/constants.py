a489354ce3b4df810e175059cab282e7a2b53c93
crvstalphua <>
2023-06-21 12:01:02 +0800

    added file and constants

--- app/constants.py
+++ app/constants.py
@@ -10,7 +10,7 @@ import streamlit as st
 
 load_dotenv(find_dotenv())
 
-DOCUMENT = "mdw-handy-guide-english-bahasa-indonesia.pdf"
+DOCUMENT = "Combined_MDW_OCR_16Jun23.pdf"
 INDEX_NAME = os.getenv('INDEX_NAME')
 MODEL_NAME = 'gpt-3.5-turbo'
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
