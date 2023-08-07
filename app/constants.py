626cb72c2fc0b7da5714a31d4d2a6c4f89246cbd
Mel_NLY <mnly0814@gmail.com>
2023-08-07 16:29:18 +0800

    run local

--- app/constants.py
+++ app/constants.py
@@ -13,15 +13,16 @@ load_dotenv(find_dotenv())
 DOCUMENT = "Combined_MDW_OCR_16Jun23.pdf"
 MODEL_NAME = 'gpt-3.5-turbo'
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-TEMPERATURE = 0
+TEMPERATURE = 0 # Set it to 0 for the most conservative answers that are unlikely to deviate from the sources.
 KENDRA_INDEX_ID = os.getenv("KENDRA_INDEX_ID")
 AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 
-AWS_DEFAULT_REGION == st.secrets["AWS_DEFAULT_REGION"]
-AWS_SECRET_ACCESS_KEY == st.secrets["AWS_SECRET_ACCESS_KEY"]
-AWS_ACCESS_KEY_ID == st.secrets["AWS_ACCESS_KEY_ID"]
-OPENAI_API_KEY == st.secrets["OPENAI_API_KEY"]
-KENDRA_INDEX_ID == st.secrets["KENDRA_INDEX_ID"]
+## Commented out to run locally
+# AWS_DEFAULT_REGION == st.secrets["AWS_DEFAULT_REGION"]
+# AWS_SECRET_ACCESS_KEY == st.secrets["AWS_SECRET_ACCESS_KEY"]
+# AWS_ACCESS_KEY_ID == st.secrets["AWS_ACCESS_KEY_ID"]
+# OPENAI_API_KEY == st.secrets["OPENAI_API_KEY"]
+# KENDRA_INDEX_ID == st.secrets["KENDRA_INDEX_ID"]
 
