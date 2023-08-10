20103b870eb59205ca7d30b45d375d5fa14f22e2
Mel_NLY <mnly0814@gmail.com>
2023-08-10 12:05:25 +0800

    included feedback loop based on queryid

--- app/function.py
+++ app/function.py
@@ -1,5 +1,7 @@
 # standard library modules
 import os
+import boto3
+from botocore.config import Config
 
 # third-party modules
 import streamlit as st
@@ -7,15 +9,21 @@ from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.retrievers import AmazonKendraRetriever
 from langchain.prompts import PromptTemplate
-
+# from langchain.memory import ConversationBufferMemory
 
 # local modules
 from constants import (
     MODEL_NAME,
     OPENAI_API_KEY,
     KENDRA_INDEX_ID,
-    TEMPERATURE
+    TEMPERATURE,
+    AWS_DEFAULT_REGION,
+    AWS_SECRET_ACCESS_KEY,
+    AWS_ACCESS_KEY_ID
 )
+
+kendra = boto3.client('kendra')
+
 
 # Build prompt
 condense_template= """Given the following conversation and a follow up question, 
@@ -56,14 +64,13 @@ def start_conversation():
         condense_question_prompt=CONDENSE_PROMPT,
         retriever=retriever,
         combine_docs_chain_kwargs={'prompt': QA_CHAIN_PROMPT},
-        verbose=True,
-        return_source_documents=True)
+        return_source_documents=True,
+        verbose = True)
 
     return chain
 
 
 def conversational_chat(chain, query):
-
     result = chain({"question": query, "chat_history": st.session_state['history']})
     st.session_state['history'].append((query, result["answer"]))
     output = result['answer']
@@ -72,4 +79,44 @@ def conversational_chat(chain, query):
         for d in result['source_documents']:
             output += '\n' + d.metadata['source']
     #output = result['answer'] + '\n \n Source: ' + ((result['source_documents'][0]).metadata)['source']
+    
+    # QUERY API
+    response = kendra.query(QueryText = query, IndexId = KENDRA_INDEX_ID)
+    queryid = response['QueryId']
+    st.session_state['queryid'].append(queryid)
+    tempList = []
+    for i in response['ResultItems']:
+        tempList.append(i['Id'])
+    st.session_state['resultids'].append(tempList)
+
     return output
+
+def goodFeedback(queryid, resultids):
+    relevance_value = "RELEVANT"
+    relevance_items = {}
+    for id in resultids:
+        relevance_items = {
+            "ResultId": id,
+            "RelevanceValue": relevance_value,
+        }
+
+    feedback = kendra.submit_feedback(
+        QueryId = queryid,
+        IndexId = KENDRA_INDEX_ID,
+        RelevanceFeedbackItems = [relevance_items]
+    )
+
+def badFeedback(queryid, resultids):
+    relevance_value = "NOT_RELEVANT"
+    relevance_items = {}
+    for id in resultids:
+        relevance_items = {
+            "ResultId": id,
+            "RelevanceValue": relevance_value,
+        }
+
+    feedback = kendra.submit_feedback(
+        QueryId = queryid,
+        IndexId = KENDRA_INDEX_ID,
+        RelevanceFeedbackItems = [relevance_items]
+    )
